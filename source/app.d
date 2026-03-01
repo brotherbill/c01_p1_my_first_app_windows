e151d712ccc56beeb4816ef779d15bdd0319d58c
Brother Bill Steinberg <brotherbill@mail.com>
2026-02-28 19:22:49 -0500

    Fix typo in comment about import statement

--- source/app.d
+++ source/app.d
@@ -3,7 +3,7 @@ import std.stdio;
 
 void main()
 {
-	// D language lets you surgically place the import statement, and libit library members needed.
+	// D language lets you surgically place the import statement, and library members needed.
 	// Duplicate members are permitted.
 	import std.stdio : writeln;
 
